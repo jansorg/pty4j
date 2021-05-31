817a9d89b68ee52f41682f0e4da68fbeeda293b4
jansorg <github@ja-dev.eu>
2021-05-31 20:40:33 +0200

    Javadoc

--- src/bashpro/pty4j/AdditionalPtyProcess.java
+++ src/bashpro/pty4j/AdditionalPtyProcess.java
@@ -7,8 +7,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
+ * Marks a process to be capable of the additional input and output streams to separate bashdb's commands from the
+ * debugged program's input and output.
+ *
  * @author jansorg
  */
+@SuppressWarnings("unused")
 public interface AdditionalPtyProcess {
     String PTY_PLACEHOLDER = "_DBG_PTY_";
 
